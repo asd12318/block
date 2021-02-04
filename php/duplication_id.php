572412e67357566a4c23fea1a8b3673a7c64b450
zack4525 <zack4525@gmail.com>
2021-02-04 18:37:56 +0900

    ㅁㄴㅇㄻㄴㅇㄹ

--- php/duplication_id.php
+++ php/duplication_id.php
@@ -3,21 +3,11 @@ $conn = mysqli_connect('localhost','suyeong0913','q1w2e3r41!','suyeong0913');
 
 $id = $_POST['id'];
 
-$sql = "SELECT id FROM user"
 
-if ($id == $sql){
-  echo '<script>alert('이미 존재하는 아이디입니다.');</script>';
-}
-else {
-  echo '<script>alert('사용 가능한 아이디입니다.');</script>';
-}
- ?>
-<?php 
-
- $id_check = mq("select * from member where id='$userid'");
+ $id_check = mq("SELECT * FORM user WHERE id='$id'");
  	$id_check = $id_check->fetch_array();
  	if($id_check >= 1){
- 		echo "<script>alert('이미 존재하는 아이디입니다.')</script>";
+ 		echo "이미 존재하는 아이디입니다.";
  	}else{
-         "<script>alert('사용 가능한 아이디입니다.')</script>"
+    echo "사용 가능한 아이디입니다."
  ?>
